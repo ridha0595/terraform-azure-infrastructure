0ecfb493b0e894a8b01e711041a393932d71128d
ridha0595 <ridhasalman05@gmail.com>
2024-08-27 21:32:02 -0400

    Initial commit of Terraform infrastructure

---
+++ main.tf
@@ -0,0 +1,17 @@
+# main.tf
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "Project10sa-ncpl"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
