bce152c0c8525778e67afdfb4fb00031ff758e73
ridha0595 <ridhasalman05@gmail.com>
2024-08-28 22:20:06 -0400

    Added rbac policies

---
+++ policies.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_policy_definition" "example_policy" {
+  name         = "example-policy"
+  policy_type   = "Custom"
+  mode          = "All"
+  display_name  = "Example Policy"
+  policy_rule   = jsonencode({
+    "if" = {
+      "field" = "location"
+      "equals" = "East US"
+    }
+    "then" = {
+      "effect" = "deny"
+    }
+  })
+}
+
+resource "azurerm_policy_assignment" "example_policy_assignment" {
+  name                 = "example-policy-assignment"
+  policy_definition_name = azurerm_policy_definition.example_policy.name
+  scope               = azurerm_resource_group.rg.id
+}
