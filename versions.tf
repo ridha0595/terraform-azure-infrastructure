5f927abe0db78baeb98bcd0b5749aee8d8c05207
ridha0595 <ridhasalman05@gmail.com>
2024-08-28 22:30:48 -0400

    added the version of azurerm

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"  # Use the appropriate version for your needs
+      version = "~> 4.0"  # Use the appropriate version for your needs
     }
   }
 }
