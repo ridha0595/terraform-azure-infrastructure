d2ed7ac6e62cb61604e32b020e8a8ff32f5e0f86
ridha0595 <ridhasalman05@gmail.com>
2024-08-29 23:42:20 -0400

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,3 @@ output "user_role_assignment_id" {
   value = azurerm_role_assignment.user_role_assignment.id
 }
 
-output "policy_definition_id" {
-  value = azurerm_policy_definition.example_policy.id
-}
-
-output "policy_assignment_id" {
-  value = azurerm_policy_assignment.policy_assignment.id
-}
